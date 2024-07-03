441ad314d9545912b2a292aae4c1eefda79560b6
Uqie Rachmadie <uuqkun@gmail.com>
2024-07-03 20:08:24 +0700

    Feat: Navbar

---
+++ components/Header.jsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+// Components
+import Nav from "./Nav";
+import MobileNav from "./MobileNav";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Uqie <span className="text-accent">.</span>
+          </h1>
+        </Link>
+        {/* Desktop nav */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div>
+        {/* Mobile nav */}
+        <div className="block xl:hidden">
+          <MobileNav />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ components/MobileNav.jsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import Link from "next/link";
+import { CiMenuFries } from "react-icons/ci";
+
+const links = [
+  { name: "home", path: "/" },
+  { name: "services", path: "/services" },
+  { name: "resume", path: "/resume" },
+  { name: "work", path: "/work" },
+  { name: "contact", path: "/contact" },
+];
+
+const MobileNav = () => {
+  const pathname = usePathname();
+
+  return (
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        {/* Logo */}
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h1>
+              Uqie <span className="text-accent"></span>.
+            </h1>
+          </Link>
+        </div>
+        {/* nav */}
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link) => {
+            return (
+              <Link
+                key={link.name}
+                href={link.path}
+                className={`${
+                  pathname === link.path && "text-accent border-b-2 border-accent"
+                } text-xl capitalize hover:text-accent transition-all`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
